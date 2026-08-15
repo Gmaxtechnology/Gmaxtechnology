508644f1baa4870d395a2ae56e77e380c09c5bca
IND <gmaxtechnology58@yahoo.com>
2026-08-15 21:48:17 +0100

    Connect GMAX to Supabase

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.GMAX_CONFIG = {
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: '',
+  SUPABASE_URL: 'https://dvmzsupknapndztzloaw.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_UPSb-FfUa5nGLjhwBQmyAQ_lATjyKK8',
   STORE_NAME: 'GMAX Technology',
   WHATSAPP_NUMBER: '2348028494730',
   PHONE_DISPLAY: '+234 802 849 4730',
