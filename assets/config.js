ca1ead2a20f3d7e512e51cd8ec2427f642a576ea
IND <gmaxtechnology58@yahoo.com>
2026-09-18 21:08:30 +0100

    New Website

--- assets/config.js
+++
@@ -1,9 +0,0 @@
-window.GMAX_CONFIG = {
-  SUPABASE_URL: 'https://dvmzsupknapndztzloaw.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_UPSb-FfUa5nGLjhwBQmyAQ_lATjyKK8',
-  STORE_NAME: 'GMAX Technology',
-  WHATSAPP_NUMBER: '2348028494730',
-  PHONE_DISPLAY: '+234 802 849 4730',
-  EMAIL: 'info@gmaxtechnology.com',
-  ADDRESS: '10, Oremeji Street, Off Simbiat Abiola Street, Ikeja, Lagos, Nigeria'
-};
--- assets/data.js
+++
@@ -1,1 +0,0 @@
-window.GMAX_PRODUCTS = [];
